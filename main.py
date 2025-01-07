aa5e7fb7bd5bf42b63058244583118f1e1e3a674
Maksym Sevkovych <maksym.sevkovych@seedbox-ventures.com>
2025-01-07 15:14:55 +0100

    NO-ISSUE: wip

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import os
 from pathlib import Path
+from pydub import AudioSegment
 
 import moviepy.editor as editor
-import torch
 from faster_whisper import WhisperModel
 
 VIDEO_DIR_PATH = "./videos"
 AUDIO_DIR_PATH = "./audios"
 TEXT_DIR_PATH = "./texts"
-DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"
+DEVICE = "cpu"
 
 
 def MP4ToMP3(mp4: str, mp3: str) -> None:
     file_to_convert = editor.AudioFileClip(mp4)
     file_to_convert.write_audiofile(mp3)
     file_to_convert.close()
+
+
+def M4AToMP3(m4a: str, mp3: str) -> None:
+    m4a_audio = AudioSegment.from_file(m4a, format="m4a")
+    m4a_audio.export(mp3, format="mp3")
 
 
 def transcribe_audio(model, mp3, text_file) -> None:
@@ -47,9 +52,23 @@ def convert_mp4_to_mp3():
         MP4ToMP3(mp4_filepath, mp3_filepath)
 
 
+def convert_m4a_to_mp3():
+    for file in os.listdir(VIDEO_DIR_PATH):
+        if not file.endswith(".m4a"):
+            continue
+        filename = Path(file).stem
+        print(f"converting file: {filename}")
+
+        m4a_filepath = os.path.join(VIDEO_DIR_PATH, file)
+        mp3_filepath = os.path.join(AUDIO_DIR_PATH, f"{Path(file).stem}.mp3")
+
+        M4AToMP3(m4a_filepath, mp3_filepath)
+
+
 def main() -> None:
     # convert videos
     convert_mp4_to_mp3()
+    convert_m4a_to_mp3()
 
     # if converted, transcribe audio:
     model = WhisperModel("large-v2", device=DEVICE, compute_type="int8")
